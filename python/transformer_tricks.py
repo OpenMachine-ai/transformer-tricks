20cf9cb119f55ec8cbd5732e2efa3a802b72f572
Matthew Clapp <itsayellow+dev@gmail.com>
2024-10-04 16:00:26 -0300

    Fixed indent and typo.

--- python/transformer_tricks.py
+++ python/transformer_tricks.py
@@ -118,60 +118,60 @@ def hello_world(repo, max_new_tok=4):
 def perplexity(repo, speedup=1, bars=False):
   """calculate perplexity of an LLM with wikitext2
   this def is copied from https://huggingface.co/docs/transformers/perplexity
-    I made the following changes to adapt it for SmolLM (was GPT2 before):
-      - changed model and tokenizer
-      - changed 'from transformers import' to point to 'Auto*' (was 'GTP2*' before)
-      - changed 'max_length' to 'config.max_position_embeddings'
-      - changed 'device' from 'cuda' to 'cpu'
-      - changed 'stride' to be 'max_length' (was 512 or 'max_length//2' before)
-      - removed 'with torch.no_grad()' and added global 'torch.set_grad_enabled(False)'
-    Perhaps a simpler and cleaner way is given here:
-    https://huggingface.co/spaces/evaluate-metric/perplexity"""
+  I made the following changes to adapt it for SmolLM (was GPT2 before):
+    - changed model and tokenizer
+    - changed 'from transformers import' to point to 'Auto*' (was 'GTP2*' before)
+    - changed 'max_length' to 'config.max_position_embeddings'
+    - changed 'device' from 'cuda' to 'cpu'
+    - changed 'stride' to be 'max_length' (was 512 or 'max_length//2' before)
+    - removed 'with torch.no_grad()' and added global 'torch.set_grad_enabled(False)'
+  Perhaps a simpler and cleaner way is given here:
+  https://huggingface.co/spaces/evaluate-metric/perplexity"""
 
-    torch.set_grad_enabled(False)  # speed up torch
-    # TODO: consider using instead "with torch.no_grad():"
+  torch.set_grad_enabled(False)  # speed up torch
+  # TODO: consider using instead "with torch.no_grad():"
 
-    tok = AutoTokenizer.from_pretrained(repo)
-    model = AutoModelForCausalLM.from_pretrained(repo, low_cpu_mem_usage=True)
+  tok = AutoTokenizer.from_pretrained(repo)
+  model = AutoModelForCausalLM.from_pretrained(repo, low_cpu_mem_usage=True)
 
-    print(f\n"perplexity('{repo}', speedup={speedup}, bars={bars})")
+  print(f"\nperplexity('{repo}', speedup={speedup}, bars={bars})")
 
-    # tokenize wikitext2
-    test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-    encodings = tok("\n\n".join(test["text"]), return_tensors="pt")
-    del tok
-    gc.collect()  # run garbage collection
+  # tokenize wikitext2
+  test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+  encodings = tok("\n\n".join(test["text"]), return_tensors="pt")
+  del tok
+  gc.collect()  # run garbage collection
 
-    max_length = model.config.max_position_embeddings
-    stride = max_length  # before it was 512 or max_length // 2
-    seq_len = encodings.input_ids.size(1) // speedup
+  max_length = model.config.max_position_embeddings
+  stride = max_length  # before it was 512 or max_length // 2
+  seq_len = encodings.input_ids.size(1) // speedup
 
-    start_time = time.perf_counter()
-    nlls = []
-    prev_end_loc = 0
-    for begin_loc in tqdm(range(0, seq_len, stride), disable=not bars):
-      end_loc = min(begin_loc + max_length, seq_len)
-      trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-      input_ids = encodings.input_ids[:, begin_loc:end_loc].to("cpu")
-      target_ids = input_ids.clone()
-      target_ids[:, :-trg_len] = -100
-      outputs = model(input_ids, labels=target_ids)
+  start_time = time.perf_counter()
+  nlls = []
+  prev_end_loc = 0
+  for begin_loc in tqdm(range(0, seq_len, stride), disable=not bars):
+    end_loc = min(begin_loc + max_length, seq_len)
+    trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+    input_ids = encodings.input_ids[:, begin_loc:end_loc].to("cpu")
+    target_ids = input_ids.clone()
+    target_ids[:, :-trg_len] = -100
+    outputs = model(input_ids, labels=target_ids)
 
-      # loss is calculated using CrossEntropyLoss which averages over valid labels
-      # N.B. the model only calculates loss over trg_len - 1 labels, because it
-      # internally shifts the labels to the left by 1.
-      neg_log_likelihood = outputs.loss
-      nlls.append(neg_log_likelihood)
+    # loss is calculated using CrossEntropyLoss which averages over valid labels
+    # N.B. the model only calculates loss over trg_len - 1 labels, because it
+    # internally shifts the labels to the left by 1.
+    neg_log_likelihood = outputs.loss
+    nlls.append(neg_log_likelihood)
 
-      prev_end_loc = end_loc
-      if end_loc == seq_len:
-        break
+    prev_end_loc = end_loc
+    if end_loc == seq_len:
+      break
 
-    ppl = torch.exp(torch.stack(nlls).mean())
-    print(f"ppl: {ppl}  time (s): {time.perf_counter()-start_time:.3f}")
-    # print('nlls:', nlls)
-    del model
-    gc.collect()  # run garbage collection
+  ppl = torch.exp(torch.stack(nlls).mean())
+  print(f"ppl: {ppl}  time (s): {time.perf_counter()-start_time:.3f}")
+  # print('nlls:', nlls)
+  del model
+  gc.collect()  # run garbage collection
 
 
 #-------------------------------------------------------------------------------------
